a71830c893593a763fee555992926524d45c226c
Jialiang Cao <jcao2080@gmail.com>
2025-06-02 16:10:57 -0400

    Implement ButtonStyle

--- Blind Navigator/Views/ScaleButtonStyle.swift
+++ Blind Navigator/Views/ScaleButtonStyle.swift
@@ -1,8 +1,17 @@
 //
-//  ScaleButtonAnimation.swift
+//  ScaleButtonStyle.swift
 //  Blind Navigator
 //
 //  Created by Jialiang Cao on 6/2/25.
 //
 
-import Foundation
+import SwiftUI
+
+public struct ScaleButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.96 : 1.0)
+            .animation(.easeOut(duration: 0.15), value: configuration.isPressed)
+    }
+}
+
